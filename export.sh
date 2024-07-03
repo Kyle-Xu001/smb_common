ff76f32ab55a20713d42f5f69e46606eda0619a7
Seangottarun <sean.wu20s@outlook.com>
2024-07-03 20:02:34 +0200

    Fix export.sh for map.pcd and detections csv

--- export.sh
+++ export.sh
@@ -3,7 +3,8 @@
 now="$(date +"%F-%H-%M-%S")"
 today="$(date +"%F")"
 
-outpath="$HOME/rss_export/${today}/${now}/
+outpath="$HOME/rss_export/${today}/${now}/"
+echo $outpath
 mkdir -p $outpath
 
 # Copy tare planner config
@@ -11,4 +12,7 @@ mkdir -p $outpath
 
 # Copy map.pcd
 scp -r team1@10.0.1.5:/home/team1/smb_ws/src/core/smb_slam/data/maps/map.pcd ${outpath}/map.pcd
+
+# Copy obj det csv
+scp -r team1@10.0.1.5:/home/team1/smb_ws/src/extract_artifacts/detected_artifacts/detected_artifacts.csv ${outpath}/detected_artifacts.csv
 
